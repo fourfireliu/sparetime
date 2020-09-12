6d17698bc1af70f4c3c39c230ed0c23d730d86a2
liuyi <liuyi02@imdada.cn>
2020-09-12 10:51:59 +0800

    all init class

---
+++ sparetime-start/src/main/java/com/fourfire/sparetime/SparetimeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.fourfire.sparetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparetimeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparetimeWorkApplication.class, args);
+	}
+
+}
---
+++ sparetime-start/src/test/java/com/fourfire/sparetime/SparetimeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourfire.sparetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparetimeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
